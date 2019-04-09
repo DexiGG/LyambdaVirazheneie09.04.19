a3ae578a2543a5f2dc1f50c8c443cca19a39737e
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-04-09 21:01:26 +0600

    ЛямбаВыражение

---
+++ Delegati/Button.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegati
+{
+    public class Button
+    {
+        public event Action<OutButtonEventArgs>ClickEvent;
+//Action не возвращает а принимает
+//Func возвращает
+//Predicate true or false 
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+    
+      
+
+        public void Click()
+        {
+            //При нажатии изменяем размер и цвет если надо
+            //if (clickButtonDelegate != null)
+            //    clickButtonDelegate(new OutButtonEventArgs());
+            ClickEvent?.Invoke(new OutButtonEventArgs());
+            //clickButtonDelegate.Invoke(new OutButtonEventArgs()); одно и тоже
+        }
+    }
+}
---
+++ Delegati/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Делегаты -button 1. ctor, methods, prop.
+            Button smallButton = new Button
+            {
+                Width = 20,
+                Height = 20,
+                Text = "Нажми меня"
+            };
+            //smallButton.ClickEvent += Hz;
+            //smallButton.ClickEvent += new ClickButtonDelegate(Hz);
+            //smallButton.ClickEvent += delegate (OutButtonEventArgs arguments)
+            //  {
+            //      return;
+            //  };
+
+
+            //ЛЯМБА ВЫРАЖЕНИЯ формула (параметры без типа через запятую) => {тело; return если надо};
+
+            smallButton.ClickEvent += (arguments) => Console.WriteLine("Меня нажали");
+
+            Button mediumButton = new Button
+            {
+                Width = 40,
+                Height = 40,
+                Text = "Нажми меня сильнее"
+            };
+
+
+            Button bigButton = new Button
+            {
+                Width = 100,
+                Height = 100,
+                Text = "Ударь меня"
+            };
+
+            List<string> names = new List<string>
+            {
+                "Бахыт",
+                "Анвар",
+                "Франсуа"
+            };
+            var result = names.Single(name => name.Contains("Ф"));
+        }
+        public static void Hz(OutButtonEventArgs args)
+        {
+            Console.WriteLine("AAAA!Меня нажали!");
+        }
+    }
+}
